c96a1624a793e837e23f11e0c474ebd70430c99e
Your Name <you@example.com>
2020-04-03 09:23:24 +0300

    last

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BigBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.csgo2 = new System.Windows.Forms.PictureBox();
+            this.fifa_20_1 = new System.Windows.Forms.PictureBox();
+            this.civi_vi_3 = new System.Windows.Forms.PictureBox();
+            this.BigBox2 = new System.Windows.Forms.PictureBox();
+            this.BigBox3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.BigBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csgo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fifa_20_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.civi_vi_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BigBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BigBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BigBox1
+            // 
+            this.BigBox1.Location = new System.Drawing.Point(12, 20);
+            this.BigBox1.Name = "BigBox1";
+            this.BigBox1.Size = new System.Drawing.Size(294, 220);
+            this.BigBox1.TabIndex = 0;
+            this.BigBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(839, 457);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 169);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(86, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fifa 20";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(385, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 51);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CS:GO";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Orange;
+            this.label3.Location = new System.Drawing.Point(709, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 51);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CIVI VI";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // csgo2
+            // 
+            this.csgo2.Image = ((System.Drawing.Image)(resources.GetObject("csgo2.Image")));
+            this.csgo2.Location = new System.Drawing.Point(56, 272);
+            this.csgo2.Name = "csgo2";
+            this.csgo2.Size = new System.Drawing.Size(185, 132);
+            this.csgo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.csgo2.TabIndex = 7;
+            this.csgo2.TabStop = false;
+            this.csgo2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.csgo2_MouseDown);
+            this.csgo2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.csgo2_MouseMove);
+            this.csgo2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.csgo2_MouseUp);
+            // 
+            // fifa_20_1
+            // 
+            this.fifa_20_1.Image = ((System.Drawing.Image)(resources.GetObject("fifa_20_1.Image")));
+            this.fifa_20_1.Location = new System.Drawing.Point(666, 272);
+            this.fifa_20_1.Name = "fifa_20_1";
+            this.fifa_20_1.Size = new System.Drawing.Size(173, 132);
+            this.fifa_20_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fifa_20_1.TabIndex = 8;
+            this.fifa_20_1.TabStop = false;
+            this.fifa_20_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.fifa_20_1_MouseDown);
+            this.fifa_20_1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.fifa_20_1_MouseMove);
+            this.fifa_20_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.fifa_20_1_MouseUp);
+            // 
+            // civi_vi_3
+            // 
+            this.civi_vi_3.Image = ((System.Drawing.Image)(resources.GetObject("civi_vi_3.Image")));
+            this.civi_vi_3.Location = new System.Drawing.Point(351, 272);
+            this.civi_vi_3.Name = "civi_vi_3";
+            this.civi_vi_3.Size = new System.Drawing.Size(199, 132);
+            this.civi_vi_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.civi_vi_3.TabIndex = 9;
+            this.civi_vi_3.TabStop = false;
+            this.civi_vi_3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.civi_vi_3_MouseDown);
+            this.civi_vi_3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.civi_vi_3_MouseMove);
+            this.civi_vi_3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.civi_vi_3_MouseUp);
+            // 
+            // BigBox2
+            // 
+            this.BigBox2.Location = new System.Drawing.Point(312, 20);
+            this.BigBox2.Name = "BigBox2";
+            this.BigBox2.Size = new System.Drawing.Size(305, 220);
+            this.BigBox2.TabIndex = 10;
+            this.BigBox2.TabStop = false;
+            // 
+            // BigBox3
+            // 
+            this.BigBox3.Location = new System.Drawing.Point(623, 20);
+            this.BigBox3.Name = "BigBox3";
+            this.BigBox3.Size = new System.Drawing.Size(286, 220);
+            this.BigBox3.TabIndex = 11;
+            this.BigBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(845, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Score=";
+            // 
+            // timer1
+            // 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 416);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.civi_vi_3);
+            this.Controls.Add(this.fifa_20_1);
+            this.Controls.Add(this.csgo2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.BigBox1);
+            this.Controls.Add(this.BigBox2);
+            this.Controls.Add(this.BigBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.BigBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.csgo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fifa_20_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.civi_vi_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BigBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BigBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BigBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox csgo2;
+        private System.Windows.Forms.PictureBox fifa_20_1;
+        private System.Windows.Forms.PictureBox civi_vi_3;
+        private System.Windows.Forms.PictureBox BigBox2;
+        private System.Windows.Forms.PictureBox BigBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork
+{
+    public partial class Form1 : Form
+    {
+        int Score;
+        bool click;
+        public Form1()
+        {
+            InitializeComponent();
+            Score = 0;
+            click = false;
+        }
+        
+
+        private void csgo2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (click)
+            {
+                csgo2.Left = csgo2.Left + e.X - csgo2.Width / 2; ;
+                csgo2.Top = csgo2.Top + e.Y - csgo2.Height / 2; ;
+                bool x1 = csgo2.Left > BigBox2.Left;
+                bool x2 = csgo2.Top > BigBox2.Top;
+                bool x3 = csgo2.Left < BigBox2.Left + BigBox2.Width - csgo2.Width;
+                bool x4 = csgo2.Top < BigBox2.Top + BigBox2.Height - csgo2.Height;
+                if (x1 && x2 && x3 && x4)
+                {
+                    csgo2.Left = BigBox2.Left + BigBox2.Width / 2 - csgo2.Width / 2;
+                    csgo2.Top = BigBox2.Top + BigBox2.Height / 2 - csgo2.Height / 2;
+                    csgo2.Enabled = false;
+                    Score++;
+                    label4.Text = "Score=" + Score;
+                }
+            }
+        }
+        private void csgo2_MouseDown(object sender, MouseEventArgs e)
+        {
+            click = true;
+        }
+
+        private void csgo2_MouseUp(object sender, MouseEventArgs e)
+        {
+            click = false;
+        }
+
+        private void civi_vi_3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (click)
+            {
+                civi_vi_3.Left = civi_vi_3.Left + e.X - civi_vi_3.Width / 2; ;
+                civi_vi_3.Top = civi_vi_3.Top + e.Y - civi_vi_3.Height / 2; ;
+                bool x1 = civi_vi_3.Left > BigBox3.Left;
+                bool x2 = civi_vi_3.Top > BigBox3.Top;
+                bool x3 = civi_vi_3.Left < BigBox3.Left + BigBox3.Width - civi_vi_3.Width;
+                bool x4 = civi_vi_3.Top < BigBox3.Top + BigBox3.Height - civi_vi_3.Height;
+                if (x1 && x2 && x3 && x4)
+                {
+                    civi_vi_3.Left = BigBox3.Left + BigBox3.Width / 2 - civi_vi_3.Width / 2;
+                    civi_vi_3.Top = BigBox3.Top + BigBox3.Height / 2 - civi_vi_3.Height / 2;
+                    civi_vi_3.Enabled = false;
+                    Score++;
+                    label4.Text = "Score=" + Score;
+                }
+            }
+
+        }
+
+        private void civi_vi_3_MouseDown(object sender, MouseEventArgs e)
+        {
+            click = true;
+        }
+
+        private void civi_vi_3_MouseUp(object sender, MouseEventArgs e)
+        {
+            click = false;
+        }
+
+        private void fifa_20_1_MouseMove(object sender, MouseEventArgs e)
+        {
+            fifa_20_1.Left = fifa_20_1.Left + e.X - fifa_20_1.Width / 2; ;
+            fifa_20_1.Top = fifa_20_1.Top + e.Y - fifa_20_1.Height / 2; ;
+            bool x1 = fifa_20_1.Left > BigBox1.Left;
+            bool x2 = fifa_20_1.Top > BigBox1.Top;
+            bool x3 = fifa_20_1.Left < BigBox1.Left + BigBox1.Width - fifa_20_1.Width;
+            bool x4 = fifa_20_1.Top < BigBox1.Top + BigBox1.Height - fifa_20_1.Height;
+            if (x1 && x2 && x3 && x4)
+            {
+                fifa_20_1.Left = BigBox1.Left + BigBox1.Width / 2 - fifa_20_1.Width / 2;
+                fifa_20_1.Top = BigBox1.Top + BigBox1.Height / 2 - fifa_20_1.Height / 2;
+                fifa_20_1.Enabled = false;
+                Score++;
+                label4.Text = "Score=" + Score;
+            }
+        
+        }
+
+        private void fifa_20_1_MouseDown(object sender, MouseEventArgs e)
+        {
+            click = true;
+        }
+
+        private void fifa_20_1_MouseUp(object sender, MouseEventArgs e)
+        {
+            click = false;
+        }
+    }
+}
